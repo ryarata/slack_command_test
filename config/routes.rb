5df030d0be3957ff2a4312a89196d6d3eaea0498
ryo arata <arata@v-force.co.jp>
2018-11-12 17:18:27 +0900

    daily_report

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'daily_report' => "command#daily_report"
+  match 'daily_report' => "command#daily_report",via: [:get,:post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
